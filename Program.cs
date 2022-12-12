fe6682b7d6ce93729adb75a26b1303435354b1a6
David <david@gmail.com>
2022-12-11 20:12:59 -0500

    2do commit

--- Program.cs
+++ Program.cs
@@ -1,23 +1,42 @@
-﻿namespace Lab02_RecursivoFibonacci;
+﻿using System.Diagnostics;
+using System;
+
+namespace Lab02_RecursivoFibonacci;
 class Program
 {
     static void Main(string[] args)
     {
+        Stopwatch stopwatch_rec = new Stopwatch();
+        Stopwatch stopwatch_it = new Stopwatch();
+
         int n;
+        
         do {
             Console.WriteLine("Introduzca el número de términos: ");
             n = int.Parse(Console.ReadLine());
         }while(n <= 1);
 
         Console.WriteLine("Serie de números de fibonnacci recursivo: ");
-
+        
+        // .........RECURSIVO..........
+        stopwatch_rec.Start();
         for (int i = 0; i < n; i++)
         {
             Console.Write("  " + FibonacciRecursivo(i));
         }
+        Console.WriteLine("\n"+"*** Recursivo -> Elapsed time: "+ stopwatch_rec.Elapsed.ToString() + " ***");
+        stopwatch_rec.Stop();
+        // ...................
+
+        // .........ITERATIVO..........
         Console.WriteLine();
         Console.WriteLine("Serie de números de fibonnacci iterativo: ");
+        
+        stopwatch_it.Start();
         FibonacciIterativo(n);
+        Console.WriteLine("\n"+"*** Iterativo -> Elapsed time: "+ stopwatch_it.Elapsed.ToString() + " ***");
+        stopwatch_it.Stop();
+        // ...................
 
     }
 
