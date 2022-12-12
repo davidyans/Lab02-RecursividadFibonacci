170f84e7faa061337583ed862c6c6ff2acbd4aba
David <david@gmail.com>
2022-12-11 19:45:20 -0500

    1er commit

--- Program.cs
+++ Program.cs
@@ -3,6 +3,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n;
+        do {
+            Console.WriteLine("Introduzca el número de términos: ");
+            n = int.Parse(Console.ReadLine());
+        }while(n <= 1);
+
+        Console.WriteLine("Serie de números de fibonnacci recursivo: ");
+
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write("  " + FibonacciRecursivo(i));
+        }
+        Console.WriteLine();
+        Console.WriteLine("Serie de números de fibonnacci iterativo: ");
+        FibonacciIterativo(n);
+
+    }
+
+    static long FibonacciRecursivo(int n){
+        if(n < 2) return n;
+        else return FibonacciRecursivo(n-2) + FibonacciRecursivo(n-1);
+    }
+
+    static void FibonacciIterativo(int n){
+        int ant1 = 0, ant2 = 1;
+        int actual = 0;
+        for (int i=1; i<=n; i++) { 
+            Console.Write("  " + actual);
+            actual = ant1 + ant2; 
+            ant2 = ant1; 
+            ant1 = actual; 
+	    }
+            Console.WriteLine();
+
     }
 }
